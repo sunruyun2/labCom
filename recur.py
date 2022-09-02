b44010d9caee27bc41cd0666a74af96386b83987
donut <31880770+sunruyun2@users.noreply.github.com>
2022-09-02 20:26:59 +0100

    Add files via upload

--- recur.py
+++ recur.py
@@ -5,3 +5,33 @@ def sumOfNatureNumber(num):
     return num + sumOfNatureNumber(num - 1)
 
 print(sumOfNatureNumber(10))
+
+
+def binarySearch(myList , left, right, x):
+    if left > right:
+        return -1
+
+    mid = left + (right - left) // 2
+
+    if mid == x:
+        return mid
+    
+    if x < mid:
+        return binarySearch(myList , left, mid -1, x)
+
+    return binarySearch(myList , mid+1 , right, x)
+
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+
+    return fib(n-1) + fib(n -2)
+
+print(fib(3))
+
+
+def mergeSort(myList):
+
+    mid = len(myList) // 2
+
+    
